edc0f832714ca4d40c24d3ac8ffde05fc8acfd3b
Iran Trinidad <devlewiso@gmail.com>
2024-10-26 19:50:24 -0600

    header updated

--- app/components/header.js
+++ app/components/header.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link'; // Importa Link de next/link
-import '../styles/Header.css'; // Asegúrate de que la ruta sea correcta
+import '../styles/header.css'; // Asegúrate de que la ruta sea correcta
 
 const Header = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
